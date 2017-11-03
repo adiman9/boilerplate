d9cc13e1047d1a1956e68d3ef2b216848cc3cf66
Adrian Henry <adrian@headspin.io>
2017-11-03 15:30:30 +0000

    add an addon to support typescript

---
+++ webpackScaffold/base/config/addons/webpack.typescript.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader"   },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader"   }
+    ]
+  },
+}
